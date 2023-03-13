192b2ba1b1a543da1b516b10f25bb0d9e4d9dee8
Rustam-Fozilov <fozilovrustam32@gmail.com>
2023-03-13 22:00:40 +0300

    configured for vue.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from "./router";
+import axios from "axios";
+
+const app = createApp(App);
+
+app
+    .use(router)
+    .mount('#app');
